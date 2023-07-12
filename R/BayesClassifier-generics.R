32d7120f34175ef2040a0b2f87542b2f81481924
sschrunner <stefan.schrunner@fys.uio.no>
2023-07-12 18:36:37 +0200

    documented generics

--- R/BayesClassifier-generics.R
+++ R/BayesClassifier-generics.R
@@ -1,3 +1,8 @@
+#' BayesClassifier generics
+#' @description Generic functions implemented for the BayesClassifier class
+#' @param x a BayesClassifier object
+#' @param ... currently unused
+#' @describeIn print.BayesClassifier prints the BayesClassifier
 #' @importFrom utils str
 #' @export
 print.BayesClassifier <- function(x, ...){
@@ -5,6 +10,8 @@ print.BayesClassifier <- function(x, ...){
   print(str(x$param))
 }
 
+#' @describeIn print.BayesClassifier summarizes the BayesClassifier
+#' @param object a BayesClassifier object
 #' @importFrom stats coef
 #' @importFrom knitr kable
 #' @export
@@ -22,21 +29,25 @@ summary.BayesClassifier <- function(object, ...){
   )
 }
 
+#' @describeIn print.BayesClassifier returns the list of model parameters in the BayesClassifier
 #' @export
 coef.BayesClassifier <- function(object, ...){
   return(object$param)
 }
 
+#' @describeIn print.BayesClassifier returns the number of classes in the BayesClassifier
 #' @export
 dim.BayesClassifier <- function(x){
   return(length(x$param))
 }
 
+#' @describeIn print.BayesClassifier returns the formula of the BayesClassifier
 #' @export
 formula.BayesClassifier <- function(x, ...){
   return(x$formula)
 }
 
+#' @describeIn print.BayesClassifier returns the log-likelihood of the BayesClassifier
 #' @export
 logLik.BayesClassifier <- function(object, ...){
   return(object$logLik)
